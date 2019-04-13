2339811fb822c783466c2996b441245560bb7c5b
Ben Parisot <me@benparisot.com>
2019-04-13 13:14:14 -0700

    add url to server.js for routing

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const url = require('url');
 
-http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World!');
-  response.end();
-})
-  .listen(8888);
+function start() {
+  function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
+    console.log(`Request for ${pathname} received.`);
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello world');
+    response.end();
+  }
+  http.createServer(onRequest)
+    .listen(8888);
+  console.log('server has started');
+}
+
+module.exports = start;
