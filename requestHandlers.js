d5300bc86e2fb1c25afce9c05956db31c9917e06
Ben Parisot <me@benparisot.com>
2019-04-14 11:03:43 -0700

    get querystring parsing postData

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,33 @@
-function start() {
+const querystring = require('querystring');
+
+function start(response, postData) {
   console.log('Request hanlder START was called.');
+
+  const body = `
+    <html>  
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        </head>
+        <body>
+            <form action="/upload" method="post">
+                <textarea name="text" rows="20" cols="60"></textarea>
+                <input type="submit" value="Submit text" />
+            </form>
+        </body>
+    </html>
+  `;
+
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(body);
+  response.end();
+
 }
 
-function upload() {
+function upload(response, postData) {
   console.log('Request handler UPLOAD was called.');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write(`You uploaded: ${querystring.parse(postData).text}`);
+  response.end();
 }
 
 
